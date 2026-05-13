23732289e1e56893dae6da0dbf85c6b48bd8fb6b
Umesh Kumar <106955266+Umesh-08@users.noreply.github.com>
2026-05-13 17:03:17 +0530

    Add files via upload
    Added linear search array program in C++

---
+++ linearserach.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int i, n, search, found = 0;
+
+    cout << "Enter the array size: ";
+    cin >> n;
+
+    vector<int> a(n);
+
+    cout << "Enter " << n << " elements: ";
+
+    for(i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << "Enter the element to search: ";
+    cin >> search;
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == search)
+        {
+            cout << "Element found at index = " << i << endl;
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        cout << "Element not found in the array";
+    }
+
+    return 0;
+}
